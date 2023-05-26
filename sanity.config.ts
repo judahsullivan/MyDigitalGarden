aaad961f3d0a699840ba9df92e8ef711b0c7cd57
Judah Sullivan <106212053+judahsullivan@users.noreply.github.com>
2023-05-26 12:40:30 -0500

    Update sanity.config.ts
    Testing base url from local environment to production to fix update issue in production

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,7 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: 'https://devjbyrd.com/studio',
   name: 'Judah_Digital_Studio',
   title: 'Judahs Digital Garden Studio',
   projectId,
